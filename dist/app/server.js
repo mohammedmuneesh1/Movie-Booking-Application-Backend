e5066c7be396759aa6f616a3ba1552320c627dbf
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2025-12-16 10:41:25 +0530

    dist added

---
+++ dist/app/server.js
@@ -0,0 +1,27 @@
+import express, {} from 'express';
+import dotenv from 'dotenv';
+import connectDB from '../config/db.js';
+dotenv.config();
+const app = express();
+console.log('NODE_ENV', process.env.NODE_ENV);
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        success: true,
+        response: "hello world 💘🫂"
+    });
+});
+//for local env
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+else {
+    connectDB().catch((error) => {
+        console.error('Failed to connect to MongoDB:', error);
+    });
+}
+//for vercel deploy
+export default app;
+//# sourceMappingURL=server.js.map
